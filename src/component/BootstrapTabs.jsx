2c0fbc5287fc96d4653e2442202c6c5211e88cfe
Shuvo Mahamud <shuvomahamud@gmail.com>
2023-06-21 20:13:29 -0400

    Formatted the document

--- src/component/BootstrapTabs.jsx
+++ src/component/BootstrapTabs.jsx
@@ -4,7 +4,7 @@ import DynamicForm from './DynamicForm';
 import TableForm from './TableForm';
 
 function BootstrapTabs() {
-      let generalBuildingInfo = [
+    let generalBuildingInfo = [
         { name: 'AEA_Project_ID', type: 'text' },
         { name: 'Property_Name', type: 'text' },
         { name: 'Address', type: 'text' },
@@ -28,8 +28,8 @@ function BootstrapTabs() {
         { name: 'Nat_Gas_Metering', type: 'text' },
         { name: 'Cooking_Metering', type: 'text' },
         { name: 'General_Notes', type: 'text' },
-      ];
-      const numericFields = [
+    ];
+    const numericFields = [
         { name: 'Zip_code', type: 'number' },
         { name: 'Built_Year', type: 'number' },
         { name: 'Number_of_Floors', type: 'number' },
@@ -39,56 +39,56 @@ function BootstrapTabs() {
         { name: 'Number_of_Heated_Floors', type: 'number' },
         { name: 'Est_Height_per_Floor_ft', type: 'number' },
         { name: 'Azimuth_of_North_Face', type: 'number' },
-      ];
+    ];
 
-      const facadeFields = [
+    const facadeFields = [
         { name: 'FD_Id', type: 'text' },
         { name: 'Length', type: 'number' },
         { name: 'Width', type: 'number' },
         { name: 'Shape', type: 'text' },
-      ];
+    ];
 
-      const facadeFieldsNumberic = [
-        'Length','Width', 'BuildingId',
-      ];
-      
-      const roofTopFields = [
+    const facadeFieldsNumberic = [
+        'Length', 'Width', 'BuildingId',
+    ];
+
+    const roofTopFields = [
         { name: 'RD_ID', type: 'text' },
         { name: 'Length', type: 'number' },
         { name: 'Width', type: 'number' },
-      ];
+    ];
 
-      const roofTopFieldsNumberic = [
-        'Length','Width', 'BuildingId',
-      ];
+    const roofTopFieldsNumberic = [
+        'Length', 'Width', 'BuildingId',
+    ];
 
-      const wall= [        
+    const wall = [
         { name: 'Id', type: 'text' },
-      { name: 'Length', type: 'number' },
-      { name: 'Width', type: 'number' },];
+        { name: 'Length', type: 'number' },
+        { name: 'Width', type: 'number' },];
 
-      const tableName = "buildings";
-      return (
+    const tableName = "buildings";
+    return (
         <Tabs defaultActiveKey="general" id="uncontrolled-tab-example" className="mb-3">
-          <Tab eventKey="general" title="General Info">
-            <DynamicForm fields={generalBuildingInfo} numericFields={numericFields} apiUrl="http://127.0.0.1/webservices/connect.php" tableName={tableName} />
-          </Tab>
-          <Tab eventKey="roofCalc" title="Roof Calc">
-            <TableForm fields={facadeFields} numericFields={facadeFieldsNumberic} apiUrl="http://127.0.0.1/webservices/roofCalc.php" tableName="facadeDeduction" />
-            <TableForm fields={roofTopFields} numericFields={roofTopFieldsNumberic} apiUrl="http://127.0.0.1/webservices/roofCalc.php" tableName="rooftopDeduction" />
-          </Tab>
-          <Tab eventKey="wallCalc" title="Wall Calc">
-            <TableForm fields={wall} numericFields={facadeFieldsNumberic} apiUrl="http://127.0.0.1/webservices/roofCalc.php" tableName="northWall" />
-            <TableForm fields={wall} numericFields={facadeFieldsNumberic} apiUrl="http://127.0.0.1/webservices/roofCalc.php" tableName="southWall" />
-            <TableForm fields={wall} numericFields={facadeFieldsNumberic} apiUrl="http://127.0.0.1/webservices/roofCalc.php" tableName="eastWall" />
-            <TableForm fields={wall} numericFields={facadeFieldsNumberic} apiUrl="http://127.0.0.1/webservices/roofCalc.php" tableName="westWall" />
-          </Tab>
-          
-          <Tab eventKey="others" title="Other Info">
-            {/* Add your other components here */}
-          </Tab>
+            <Tab eventKey="general" title="General Info">
+                <DynamicForm fields={generalBuildingInfo} numericFields={numericFields} apiUrl="http://127.0.0.1/webservices/connect.php" tableName={tableName} />
+            </Tab>
+            <Tab eventKey="roofCalc" title="Roof Calc">
+                <TableForm fields={facadeFields} numericFields={facadeFieldsNumberic} apiUrl="http://127.0.0.1/webservices/roofCalc.php" tableName="facadeDeduction" />
+                <TableForm fields={roofTopFields} numericFields={roofTopFieldsNumberic} apiUrl="http://127.0.0.1/webservices/roofCalc.php" tableName="rooftopDeduction" />
+            </Tab>
+            <Tab eventKey="wallCalc" title="Wall Calc">
+                <TableForm fields={wall} numericFields={facadeFieldsNumberic} apiUrl="http://127.0.0.1/webservices/roofCalc.php" tableName="northWall" />
+                <TableForm fields={wall} numericFields={facadeFieldsNumberic} apiUrl="http://127.0.0.1/webservices/roofCalc.php" tableName="southWall" />
+                <TableForm fields={wall} numericFields={facadeFieldsNumberic} apiUrl="http://127.0.0.1/webservices/roofCalc.php" tableName="eastWall" />
+                <TableForm fields={wall} numericFields={facadeFieldsNumberic} apiUrl="http://127.0.0.1/webservices/roofCalc.php" tableName="westWall" />
+            </Tab>
+
+            <Tab eventKey="others" title="Other Info">
+                {/* Add your other components here */}
+            </Tab>
         </Tabs>
-      );
-    }
-    
-    export default BootstrapTabs;
+    );
+}
+
+export default BootstrapTabs;
